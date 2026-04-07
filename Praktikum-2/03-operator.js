f1ec3f0c3e0dd26d9478bd97bc4b934212260a9f
Randy <randysalim.id@gmail.com>
2026-04-07 23:10:03 +0700

    praktikum 2 selesai

---
+++ Praktikum-2/03-operator.js
@@ -0,0 +1,31 @@
+let a = 17;
+let b = 5;
+console.log('=== Aritmatika ===');
+console.log(`${a} + ${b} = ${a + b}`); 
+console.log(`${a} - ${b} = ${a - b}`); 
+console.log(`${a} * ${b} = ${a * b}`); 
+console.log(`${a} / ${b} = ${a / b}`); 
+console.log(`${a} % ${b} = ${a % b}`); 
+console.log(`${a} ** ${b} = ${a ** b}`); 
+
+console.log('=== Perbandingan ===');
+console.log('10 > 5 :', 10 > 5); 
+console.log('10 < 5 :', 10 < 5); 
+console.log('10 >= 10 :', 10 >= 10); 
+console.log('10 <= 9 :', 10 <= 9); 
+
+console.log('--- Perbedaan == dan === ---');
+console.log('5 == "5" :', 5 == '5'); 
+console.log('5 === "5" :', 5 === '5'); 
+console.log('5 !== 3 :', 5 !== 3); 
+
+console.log('=== Logika ===');
+let sudahMakan = true;
+let punyaUang = false;
+
+console.log('Makan && Uang :', sudahMakan && punyaUang); 
+
+console.log('Makan || Uang :', sudahMakan || punyaUang); 
+
+console.log('!sudahMakan :', !sudahMakan); 
+console.log('!punyaUang :', !punyaUang);
